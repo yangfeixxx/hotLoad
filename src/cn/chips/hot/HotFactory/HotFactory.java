a63dfebfee9fadd20654f5defd2de96e9ba9d060
yangfei <892895660@qq.com>
2018-07-21 16:50:42 +0800

    hotLoad

--- src/cn/chips/hot/HotFactory/HotFactory.java
+++ src/cn/chips/hot/HotFactory/HotFactory.java
@@ -5,11 +5,19 @@ import cn.chips.hot.controlCenter.HotLoadControl;
 
 //2018/7/21 cread by yangfei
 public class HotFactory {
+    private HotLoadConfiguration configuration;
+
+    public HotLoadConfiguration getConfiguration() {
+        return configuration;
+    }
+
     public HotLoadControl buildHotLoad(HotLoadConfiguration configuration) {
+        this.configuration = configuration;
         return new HotLoadControl(configuration);
     }
 
     public HotLoadControl buildHotLoad() {
-        return new HotLoadControl(new HotLoadConfiguration());
+        this.configuration = new HotLoadConfiguration();
+        return new HotLoadControl(configuration);
     }
 }
--- src/cn/chips/hot/controlCenter/HotLoadControl.java
+++ src/cn/chips/hot/controlCenter/HotLoadControl.java
@@ -4,9 +4,12 @@ package cn.chips.hot.controlCenter;
 import cn.chips.hot.configuration.HotLoadConfiguration;
 import cn.chips.hot.handler.ScanHandler;
 import cn.chips.hot.utils.StringUtils;
+
 //2018/7/21 cread by yangfei
 public class HotLoadControl {
     private HotLoadConfiguration hotLoadConfiguration;
+    private static final Long defaultInterval = 1L;
+    private static final String defaultLocation = System.getProperty("user.dir");
 
     public HotLoadControl() {
     }
@@ -26,10 +29,10 @@ public class HotLoadControl {
             hotLoadConfiguration = new HotLoadConfiguration();
 
         if (hotLoadConfiguration.getInterval() == null && hotLoadConfiguration.getInterval() <= 0) {
-            hotLoadConfiguration.setInterval(1L);
+            hotLoadConfiguration.setInterval(defaultInterval);
         }
         if (!StringUtils.isNotEmpty(hotLoadConfiguration.getLocation())) {
-            hotLoadConfiguration.setLocation(System.getProperty("user.dir"));
+            hotLoadConfiguration.setLocation(defaultLocation);
         }
 
     }
